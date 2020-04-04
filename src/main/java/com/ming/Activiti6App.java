11e4afbe1eb47191910e83a6b1e2ea20da9898fb
Chiminwon <chiminwon@gmail.com>
2020-04-04 23:22:58 +0800

    Initial Commit

---
+++ src/main/java/com/ming/Activiti6App.java
@@ -0,0 +1,15 @@
+package com.ming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ming
+ */
+@SpringBootApplication
+public class Activiti6App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activiti6App.class, args);
+	}
+}
---
+++ src/main/java/com/ming/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.ming.controller;
+
+import com.ming.entity.User;
+import com.ming.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Created by ming on 2018-01-08 13:39
+ */
+@Controller
+public class LoginController {
+
+    @Resource
+    private UserService userService;
+
+    @PostMapping("/login")
+    @ResponseBody
+    public boolean login(HttpSession session, @RequestBody User user) {
+        String userName = user.getUserName();
+        String password = user.getPassword();
+        boolean login = userService.login(userName, password);
+        if (login) {
+            session.setAttribute("userName", userName);
+            return true;
+        }
+        return false;
+    }
+
+    @GetMapping("/exit")
+    public String exit(HttpSession session) {
+        session.removeAttribute("userName");
+        return "login";
+    }
+}
